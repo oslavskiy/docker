4a55f5f5b162a436cf665b1ec64d1356d8bebac1
d.oslavskiy <oslavskiy@gmail.com>
2018-11-29 12:11:09 +0300

    small changes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            I changed something
           </a>
         </header>
       </div>
